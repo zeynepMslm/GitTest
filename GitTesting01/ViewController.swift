63b602fcbdaf866a1419ad4c3e7ba722b3be8ef1
Zeynep Müslim <zeynep.muslim@icloud.com>
2020-08-06 23:26:51 +0300

    Deneme
    deneme1

---
+++ GitTesting01/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTesting01
+//
+//  Created by Zeynep Müslim on 4.08.2020.
+//  Copyright © 2020 Zeynep Müslim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Kyadetme deneme")
+    }
+
+
+}
+
